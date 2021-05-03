9c02e37d6b0a72341c91ab62dcc3904dba4c2185
gdanielgimenez <gdanielgimenez@gmail.com>
2021-05-03 17:50:20 -0400

    fixed error for fetching location and voice input

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import TextField from '@material-ui/core/TextField';
 import { Button, Card, CardContent, CardHeader, Container, Typography, Grid, CardMedia, FormControl } from '@material-ui/core';
 import Brightness2Icon from '@material-ui/icons/Brightness2';
@@ -6,48 +6,39 @@ import BrightnessHighIcon from '@material-ui/icons/BrightnessHigh';
 import ReactAnimatedWeather from 'react-animated-weather';
 import CountUp from 'react-countup';
 import styles from './Search.module.css';
-import PublicIcon from '@material-ui/icons/Public';
+import { useSpeechContext} from '@speechly/react-client'; 
 
 
 const Search = ({weather, storm}) =>{
 //set the state with hooks
     const [location,setLocation] = useState('')
 //-------------------------------------------------
+const {segment} = useSpeechContext();
 //functions---------------------------------------------------------------------------
         const handleSubmit = (e) =>{
             e.preventDefault();
             storm(location)
             setLocation('')
         }
+        //-----------------------
         const handleChange = (e)=>{
             setLocation(document.getElementById("textSearch").value)
         }
-//--------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------
         let defaults = {
             size: 125,
             animate: true
         }
+        useEffect(()=>{
+            if(segment){
+                if(segment.intent.intent === 'weather_in' && segment.entities.[0] !== undefined && segment.isFinal   ){
+                    storm(segment.entities.[0].value);    
+                } else{
+                    console.log('try again');
+                }
+            }
+        },[segment])
         const displayWeather = 
-    /*        
-            <Card variant="elevation" className={styles.root} >         
-                        <Typography className={styles.cardTitle} variant="h6">{weather.name} <Typography variant="body1">{weather.country}</Typography><Typography variant="h6"> {weather.description}</Typography></Typography>
-                        <CardMedia className={styles.icons}  >    
-                            <Typography variant="h5"> <CountUp end={Number(weather.temp)} decimals={1} duration={6} /> °C</Typography>
-                            <ReactAnimatedWeather
-                                icon={weather.icon}
-                                color={weather.color}
-                                size={defaults.size}
-                                animate={defaults.animate}
-                            />
-                        </CardMedia> 
-                        <CardContent className={styles.data}> 
-                            <Typography variant="subtitle1"> Max : <CountUp end={Number(weather.max)} decimals={1} duration={6}/> °C</Typography>
-                            <Typography variant="subtitle1"> Min : <CountUp end={Number(weather.min)} decimals={1} duration={6}/> °C</Typography>
-                            <Typography variant="subtitle1"> Humidity : <CountUp  end={Number(weather.humidity)} duration={6}/>  %</Typography>
-                            <Typography variant="subtitle1" > Sunrise : {weather.sunriseLocal} <BrightnessHighIcon/> </Typography>
-                            <Typography variant="subtitle1">  Sunset : {weather.sunsetLocal}   <Brightness2Icon fontSize="small"/></Typography>
-                        </CardContent>   
-            </Card>*/
             <Grid xs={9}  className={styles.root} >
                       
                      <Grid item  xs={12} md={3}>
@@ -70,12 +61,25 @@ const Search = ({weather, storm}) =>{
                             <Typography variant="subtitle1">  Sunset : {weather.sunsetLocal}   <Brightness2Icon fontSize="small"/></Typography>
                                             
                     </Grid>   
-            </Grid>      
+            </Grid>
+            
+            let words = 
+            <Typography>
+            {segment && (
+                <>
+                  {segment.words.map((w)=> w.value).join(" ")};
+
+                </>
+                )} 
+            </Typography>
         return(
             <div>
                 <Container  >
                 <Typography variant="h5" className={styles.title}>Get the current weather on any city</Typography>
-                   <form  className={styles.form} onSubmit={handleSubmit} >
+                   <Typography className={styles.title}>
+                            {words}    
+                   </Typography>
+                   <form id="form" className={styles.form} onSubmit={handleSubmit} >
                     
                     <TextField  required onChange={handleChange} className={styles.outlinedRoot}  
                             id="textSearch"
@@ -84,7 +88,7 @@ const Search = ({weather, storm}) =>{
                             value={location}
                             margin="normal"
                         />
-                        <Button type="submit" variant="contained" className={styles.button}  >
+                        <Button type="submit" id="button" variant="contained" className={styles.button}  >
                         <Typography className={styles.text}> submit</Typography> 
                         </Button>
                         </form>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {SpeechProvider} from '@speechly/react-client';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <SpeechProvider appId="9534f3c5-a6a4-4155-a0e0-ed94fde9306d" language="en-US">
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </SpeechProvider>,
   document.getElementById('root')
 );
 
